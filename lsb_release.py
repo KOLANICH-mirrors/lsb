0e8d503e6530807a5b0d95e93b412654101b74e1
KOLANICH <kolan_n@mail.ru>
2022-11-29 15:24:08 +0300

    Got rid of the distinct `lsb_release` script. Moved its functionality into `lsb_release.py`.

--- lsb_release.py
+++ lsb_release.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3 -Es
 # LSB release detection module for Debian
 # (C) 2005-10 Chris Lawrence <lawrencc@debian.org>
 # (C) 2018 Didier Raboud <odyx@debian.org>
@@ -16,6 +17,7 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 #    02110-1301 USA
 
+from argparse import ArgumentParser
 import csv
 import os
 import re
@@ -369,5 +371,72 @@ def test():
     print(get_distro_information())
     print(check_modules_installed())
 
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--version', dest='version', action='store_true',
+                      default=False,
+                      help="show LSB modules this system supports")
+    parser.add_argument('-i', '--id', dest='id', action='store_true',
+                      default=False,
+                      help="show distributor ID")
+    parser.add_argument('-d', '--description', dest='description',
+                      default=False, action='store_true',
+                      help="show description of this distribution")
+    parser.add_argument('-r', '--release', dest='release',
+                      default=False, action='store_true',
+                      help="show release number of this distribution")
+    parser.add_argument('-c', '--codename', dest='codename',
+                      default=False, action='store_true',
+                      help="show code name of this distribution")
+    parser.add_argument('-a', '--all', dest='all',
+                      default=False, action='store_true',
+                      help="show all of the above information")
+    parser.add_argument('-s', '--short', dest='short',
+                      action='store_true', default=False,
+                      help="show requested information in short format")
+    
+    (options, args) = parser.parse_args()
+    if args:
+        parser.error("No arguments are permitted")
+
+    short = (options.short)
+    none = not (options.all or options.version or options.id or
+                options.description or options.codename or options.release)
+
+    distinfo = lsb_release.get_distro_information()
+
+    if none or options.all or options.version:
+        verinfo = lsb_release.check_modules_installed()
+        if not verinfo:
+            print("No LSB modules are available.", file=sys.stderr)
+        elif short:
+            print(':'.join(verinfo))
+        else:
+            print('LSB Version:\t' + ':'.join(verinfo))
+
+    if options.id or options.all:
+        if short:
+            print(distinfo.get('ID', 'n/a'))
+        else:
+            print('Distributor ID:\t%s' % distinfo.get('ID', 'n/a'))
+
+    if options.description or options.all:
+        if short:
+            print(distinfo.get('DESCRIPTION', 'n/a'))
+        else:
+            print('Description:\t%s' % distinfo.get('DESCRIPTION', 'n/a'))
+
+    if options.release or options.all:
+        if short:
+            print(distinfo.get('RELEASE', 'n/a'))
+        else:
+            print('Release:\t%s' % distinfo.get('RELEASE', 'n/a'))
+
+    if options.codename or options.all:
+        if short:
+            print(distinfo.get('CODENAME', 'n/a'))
+        else:
+            print('Codename:\t%s' % distinfo.get('CODENAME', 'n/a'))
+
 if __name__ == '__main__':
-    test()
+    main()
